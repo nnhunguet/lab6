2d4345a16d11c601233f5e69e1327c8d5840ee84
nnhunguet <52577080+nnhunguet@users.noreply.github.com>
2020-08-06 16:36:25 +0700

    lab-6

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from "react-native";
+import MapView from "react-native-maps";
+import { Marker, Callout } from 'react-native-maps'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const onLongPress = () => {
+  console.log("OnLongPress");
+};
+
+const onPressPicture = () => {
+  console.log(1);
+};
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <MapView
+        style={{
+          flex: 1
+        }}  
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        onLongPress={onLongPress}
+      >
+        <Marker 
+          coordinate={{latitude: 37.78825, longitude: -122.4324}} 
+        >
+          <Callout tooltip>
+            <View style={styles.callout}>
+              <Text>Test Call out</Text>
+              <TouchableOpacity
+                onPress={onPressPicture}
+                style={{backgroundColor: "red"}}
+              >
+                <Text>Picture</Text>
+              </TouchableOpacity>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  callout: {
+    // backgroundColor: "red",
+  }
+})
